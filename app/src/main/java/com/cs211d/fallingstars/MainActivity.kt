f410fa52a8e79278f9053400622fef8be75812f7
ming <ming@codethedream.org>
2023-11-05 14:41:07 -0800

    not fully working Step 4

--- app/src/main/java/com/cs211d/fallingstars/MainActivity.kt
+++ app/src/main/java/com/cs211d/fallingstars/MainActivity.kt
@@ -1,34 +1,43 @@
 package com.cs211d.fallingstars
 
 import android.animation.Animator
-import android.animation.AnimatorSet
+import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
-import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
-import android.view.animation.BounceInterpolator
+import android.view.animation.LinearInterpolator
 import android.widget.Button
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import androidx.core.view.size
+import java.sql.Time
 import java.util.*
 import kotlin.random.Random
 import kotlin.concurrent.timerTask
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
     private lateinit var gameSpace: FrameLayout
     private lateinit var startButton: Button
+    private lateinit var stopButton: Button
     private lateinit var resetButton: Button
     private lateinit var scoreTextView: TextView
+
+    private var startTime: Long = 0
+    private var stopTime: Long = 0
 
     private var score = 0
     private var total = 0
     private var isGameRunning = false
     private var spawnIntervalLow = 500L
     private var spawnIntervalHigh = 3000L
-
 
     private var timer: Timer? = null
 
@@ -38,21 +47,25 @@ class MainActivity : AppCompatActivity() {
 
         gameSpace = findViewById(R.id.game_space)
         startButton = findViewById(R.id.start_button)
+        stopButton = findViewById(R.id.stop_button)
         resetButton = findViewById(R.id.reset_button)
         scoreTextView = findViewById(R.id.score_textview)
         scoreTextView.text = getString(R.string.score_label, score, total)
 
         startButton.setOnClickListener { startGame() }
+        stopButton.setOnClickListener { stopGame() }
         resetButton.setOnClickListener { resetGame() }
     }
 
     private fun startGame() {
         if (!isGameRunning) {
+            startTime = System.currentTimeMillis()
             isGameRunning = true
             score = 0
             total = 0
             scoreTextView.text = getString(R.string.score_label, score, total)
             startButton.isEnabled = false
+            stopButton.isEnabled = true
 
             /* spawn falling shapes using a timer */
             // interval specifies how frequently a shape will be spawned (the spawn rate)
@@ -79,70 +92,74 @@ class MainActivity : AppCompatActivity() {
         )
 
         /* YOUR STEP 2 CODE HERE */
-        // 2A
         val random = Random.Default
-        val color: Int = random.nextInt()
-        fallingShape.setColorFilter(color)
+        val red = random.nextInt(256)
+        val green = random.nextInt(256)
+        val blue = random.nextInt(256)
 
-        gameSpace.addView(fallingShape)
+        fallingShape.setColorFilter(Color.rgb(red, green, blue))
 
-        // 2B
-        fallingShape.setOnTouchListener(object : View.OnTouchListener {
-            @SuppressLint("ClickableViewAccessibility")
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-
-                when (event!!.action) {
+        fallingShape.setOnTouchListener(object: View.OnTouchListener {
+            override fun onTouch(v: View, motionEvent: MotionEvent): Boolean {
+                when (motionEvent.action){
                     MotionEvent.ACTION_DOWN -> {
+                        gameSpace.removeView(fallingShape)
                         score += 1
                         scoreTextView.text = getString(R.string.score_label, score, total)
-                        gameSpace.removeAllViews()
-                    }
-                    MotionEvent.ACTION_MOVE -> {
-                        //
-                    }
-                    MotionEvent.ACTION_UP -> {
-                        //
                     }
                 }
                 return true
             }
         })
 
+        gameSpace.addView(fallingShape)
+
         /* YOUR STEP 3 CODE HERE */
+        val maxX = gameSpace.width - fallingShape.drawable.intrinsicWidth
+        val x = random.nextInt(maxX)
 
-        // 3A
-        val maxTranslationX = gameSpace.width - fallingShape.drawable.intrinsicWidth
-        val translationX = random.nextInt(maxTranslationX).toFloat()
-        val translateX = ObjectAnimator.ofFloat(fallingShape, View.TRANSLATION_X, translationX, translationX)
+        fallingShape.x = x.toFloat()
 
-        // 3B
-        val maxTranslationY = gameSpace.height.toFloat()
-        val translateY = ObjectAnimator.ofFloat(fallingShape, View.TRANSLATION_Y, maxTranslationY)
-        translateY.duration = 4000
-        translateY.interpolator = BounceInterpolator()
+        val y = gameSpace.height.toFloat()
+        val randomDuration = random.nextInt(5000 - 1000) + 1000
 
+        val fallDot = ObjectAnimator.ofFloat(fallingShape, "translationY", 0f, y)
+        fallDot.interpolator = LinearInterpolator()
+        fallDot.duration = randomDuration.toLong()
+        fallDot.start()
 
-        // 3C
-        val animSet = AnimatorSet()
-        animSet.play(translateX).with(translateY)
-        //animSet.play(growRocketX).after(squatRocketY).with(__B__)
-        //animSet.play(__C__).after(growRocketY)
-        animSet.start()
+        total += 1
 
-        // 3D
-        /*
-        animSet.addListener(object : Animator.AnimatorListener {
-            override fun onAnimationStart(animation: Animator) {}
-
-            override fun onAnimationEnd(animation: Animator) {
-                total += 1
+        fallDot.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator) {
                 scoreTextView.text = getString(R.string.score_label, score, total)
-                gameSpace.removeAllViews()
+            }
+            override fun onAnimationEnd(animation: Animator) {
+                gameSpace.removeView(fallingShape)
             }
         })
 
-         */
+    }
 
+    private fun stopGame() {
+        isGameRunning = false
+        stopTime = System.currentTimeMillis()
+        startButton.isEnabled = true
+        stopButton.isEnabled = false
+
+        scoreTextView.text = getString(R.string.score_label, score, total)
+
+        val pauseDuration = abs(startTime - stopTime)
+
+        for (i in 0 until gameSpace.childCount) {
+            val childView = gameSpace.getChildAt(i)
+            // Add a listener to each child view
+            ObjectAnimator.ofFloat(childView, View.Y, childView.y, childView.y)
+                .apply {
+                    duration = pauseDuration // Set the pause duration
+                    start()
+                }
+        }
     }
 
     private fun resetGame() {
